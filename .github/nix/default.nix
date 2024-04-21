bd258331cf56ee8e588a5e94bea61514e7eec534
Marvin Mann <marvin1995mann@gmail.com>
2024-04-21 17:36:20 +0200

    add usability interface to shell script test_go

--- .github/nix/default.nix
+++ .github/nix/default.nix
@@ -1,10 +1,54 @@
 { pkgs, }:
 {
-  test_go = pkgs.writeShellApplication {
-    name = "test_go";
-    runtimeInputs = [ pkgs.go ];
-    text = ''go test'';
-  };
+  test_go =
+    let
+      scriptName = "test_go";
+    in
+    pkgs.writeShellApplication {
+      name = scriptName;
+      runtimeInputs = [ pkgs.go ];
+      text = ''
+        #!${pkgs.bash}/bin/bash
+
+        # Define options string
+        options=":o:h"
+
+        # Loop through arguments using getopts
+        while getopts "$options" opt; do
+          case $opt in
+            h)  # Help option
+              echo "There is only one supported option!
+              "
+              echo "To store the test results in a specific location"
+              echo "use: ${scriptName} -o PATH/FILENAME"
+              exit 0
+              ;;
+            o)  # Argument with flag
+              echo "PATH/FILENAME pattern $OPTARG was provided"
+              echo "Generating test results..."
+              go test -json > "$OPTARG"
+              echo "File $OPTARG was created."
+              exit 0
+              ;;
+            ?)  # Invalid option
+              echo "Invalid option: -$OPTARG OR missing argument"
+              echo "To display supported options"
+              echo "use: ${scriptName} -h"
+              exit 1
+              ;;
+          esac
+        done
+
+        # No options or flag used
+        echo "No argument provided or option used."
+        echo "Generating test results..."
+        beginning="$(date +"%Y_%m_%d_%H_%M_%S")"
+        go test -json > ./"$beginning"
+        end="$(date +"%Y_%m_%d_%H_%M_%S_test_results")"
+        mv ./"$beginning" ./"$end"
+        echo "File $end was created."
+      '';
+    };
   lint_go = pkgs.writeShellApplication {
     name = "lint_go";
     runtimeInputs = [ pkgs.golangci-lint ];
